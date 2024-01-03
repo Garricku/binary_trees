fd7cd5588abb868a1b1d1ad7d396dd15da448361
Garrick Larson Potts <ricki.glp@gmail.com>
2024-01-03 04:47:33 -0800

    task 9 height function betty correction

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -17,9 +17,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	right_height = binary_tree_height(tree->right);
 
 	if (tree->left != NULL)
-		left_height ++;
+		left_height++;
 	if (tree->right != NULL)
-		right_height ++;
+		right_height++;
 	if (left_height > right_height)
 		return (left_height);
 	else
