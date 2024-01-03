95e46bad3face272dbd7e49839cc4516b3eb20a7
Garrick Larson Potts <ricki.glp@gmail.com>
2024-01-03 06:59:07 -0800

    task 15 is full function betty correction made

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -14,7 +14,7 @@ int binary_tree_is_full(const binary_tree_t *tree)
 
 	if (tree->right == NULL && tree->left == NULL)
 		return (1);
-	
+
 	if (tree->left != NULL && tree->right != NULL)
 		return (binary_tree_is_full(tree->left)
 				&& binary_tree_is_full(tree->right));
